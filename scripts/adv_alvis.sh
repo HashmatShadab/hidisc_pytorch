496a2dbb5549766bfd12f7da8fb2ee52ce78bc89
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2024-02-20 12:08:24 +0400

    Adjust total_loss calculation and update DistributedDataParallel
    Updated the total_loss calculation in train.py for attack types 'pgd' and 'pgd_2'. In main.py, added "broadcast_buffers=False" to the DistributedDataParallel function settings to prevent broadcasting of buffers during the model training to optimize

--- scripts/adv_alvis.sh
+++ scripts/adv_alvis.sh
@@ -4,16 +4,16 @@
 NUM_GPUS=$1
 BATCH_SIZE=$2
 exp_num=$3
-
+model_name=$4
 
 # if exp_num == 1, run the firstscript
 
 if [ $exp_num -eq 1 ]
 then
   echo "Running Adv Training for resnet50 with $NUM_GPUS GPUs and batch size $BATCH_SIZE and dynamic_aug=True"
-    torchrun --nproc_per_node=$NUM_GPUS --master_port="$RANDOM" main.py data.db_root=/mimer/NOBACKUP/groups/alvis_cvl/Fahad/OpenSRH data.dynamic_aug=True model.backbone=resnetv2_50 \
-    training.attack.name=pgd  training.attack.eps=8 training.batch_size=$BATCH_SIZE  out_dir=Results/Adv/resnetv2_50_attack_pgd_eps_8_dynaug_True \
-    wandb.exp_name=Adv_backbone_resnetv2_50_attack_pgd_eps_8_dynamic_aug_True wandb.use=True
+    torchrun --nproc_per_node=$NUM_GPUS --master_port="$RANDOM" main.py data.db_root=/mimer/NOBACKUP/groups/alvis_cvl/Fahad/OpenSRH data.dynamic_aug=True model.backbone=$model_name \
+    training.attack.name=pgd  training.attack.eps=8 training.batch_size=$BATCH_SIZE  out_dir=Results/Adv/${model_name}_attack_pgd_eps_8_dynaug_True \
+    wandb.exp_name=Adv_backbone_${model_name}_attack_pgd_eps_8_dynamic_aug_True wandb.use=True
 fi
 
 if [ $exp_num -eq 2 ]
