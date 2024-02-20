b0e52dcaec63c64b1a26c81c217993d78773d71f
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2024-02-20 14:02:09 +0400

    Add conditional flag for unused parameters in training script
    The script `adv_alvis.sh` was updated to ensure optimal use of computational resources in training. If the model backbone is set to "resnet50_multi_bn", a flag is now set to true in order to find and disregard any unused parameters during model training.

--- scripts/adv_alvis.sh
+++ scripts/adv_alvis.sh
@@ -8,12 +8,21 @@ model_name=$4
 
 # if exp_num == 1, run the firstscript
 
+param="False"
+# if model_backbone == resnet50_multi_bn, theen set flag to True
+if [ $model_backbone == "resnet50_multi_bn" ]
+then
+    param="True"
+fi
+
+
+
 if [ $exp_num -eq 1 ]
 then
   echo "Running Adv Training for resnet50 with $NUM_GPUS GPUs and batch size $BATCH_SIZE and dynamic_aug=True"
     torchrun --nproc_per_node=$NUM_GPUS --master_port="$RANDOM" main.py data.db_root=/mimer/NOBACKUP/groups/alvis_cvl/Fahad/OpenSRH data.dynamic_aug=True model.backbone=$model_name \
     training.attack.name=pgd  training.attack.eps=8 training.batch_size=$BATCH_SIZE  out_dir=Results/Adv/${model_name}_attack_pgd_eps_8_dynaug_True \
-    wandb.exp_name=Adv_backbone_${model_name}_attack_pgd_eps_8_dynamic_aug_True wandb.use=True
+    wandb.exp_name=Adv_backbone_${model_name}_attack_pgd_eps_8_dynamic_aug_True wandb.use=True distributed.find_unused_params=$param
 fi
 
 if [ $exp_num -eq 2 ]
