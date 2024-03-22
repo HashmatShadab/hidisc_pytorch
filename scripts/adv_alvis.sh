201494c08e596a9455f8068edced7cca69c292ff
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2024-03-22 12:55:05 +0400

    Add multi-node adv training condition
    Introduced a conditional block in `adv_alvis.sh` to run advanced training with dynamic augmentation for multi-node environments when `exp_num` equals 1234. This setup specifies the use of a random master port and sets the

--- scripts/adv_alvis.sh
+++ scripts/adv_alvis.sh
@@ -8,6 +8,13 @@ model_name=$4
 
 # if exp_num == 1, run the firstscript
 
+if [ $exp_num -eq 1234 ]
+then
+  echo "Running Adv Training with $NUM_GPUS GPUs and batch size $BATCH_SIZE and dynamic_aug=True Test for multi node training"
+    torchrun --nproc_per_node=$NUM_GPUS --master_port="$RANDOM" main.py data.db_root=/mimer/NOBACKUP/groups/alvis_cvl/Fahad/OpenSRH data.dynamic_aug=True model.backbone=$model_name \
+    training.attack.name=pgd  training.attack.eps=8 training.batch_size=$BATCH_SIZE  out_dir=Results/Adv/${model_name}_attack_pgd_eps_8_dynaug_True_multinode \
+    wandb.exp_name=Adv_backbone_${model_name}_attack_pgd_eps_8_dynamic_aug_True_multinode wandb.use=True
+fi
 
 if [ $exp_num -eq 1 ]
 then
