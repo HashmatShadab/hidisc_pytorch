2f09e7d96477bc6ee62b988579d1edfd3ba66d42
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2024-09-04 23:47:32 +0400

    update

--- scripts/eval_knn.sh
+++ scripts/eval_knn.sh
@@ -3,14 +3,21 @@
 DATA_PATH=/mimer/NOBACKUP/groups/alvis_cvl/Fahad/OpenSRH
 
 model_name=$1
-ckpt_path=$2
+ckpt_dir=$2
 
-ckpt_dir=$(dirname $PWD/$ckpt_path)
 echo "ckpt_dir: $ckpt_dir"
 
 
-
 echo "saving results to: $ckpt_dir/eval_knn_results"
+
+
+# loop over all the checkpoints in the directory ending with .pth
+for ckpt_path in $ckpt_dir/*.pth; do
+
+    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+done
+
 #
-python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name \
---eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
