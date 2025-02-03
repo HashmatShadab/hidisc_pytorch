a0e4aba693037ac9e1a8eb2151676bc7fa6c8856
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2025-02-03 12:51:14 +0400

    update

--- scripts/eval_adv_knn.sh
+++ scripts/eval_adv_knn.sh
@@ -80,7 +80,101 @@ model_name=$1
 
 
 ######################################################################################################################
+#Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_exp20
+#Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_exp31_with_adv_loss_pt
+#Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_exp32_with_adv_loss_s_pt
+#Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_patch_loss_exp28
+#Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_proj_head_exp29
+#Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_proj_head_exp30
+#Results/Adv/resnet50_dynamicaug_true_epsilon_warmup_5000_exp20
+#Results/Adv/resnet50_dynamicaug_true_epsilon_warmup_5000_only_adv_patch_loss_exp28
+#Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_exp20
+#Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_exp31_with_adv_loss_pt
+#Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_exp32_with_adv_loss_s_pt
+#Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_patch_loss_exp28
+#Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_proj_head_exp29
+#Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_proj_head_exp30
 
+###################### Exp 18 ########################################
+echo "Exp 18"
+ckpt_dir="Results/Baseline/resnet50_timm_pretrained_exp18"
+
+echo "ckpt_dir: $ckpt_dir"
+echo "saving results to: $ckpt_dir/eval_knn_results"
+
+# loop over all the checkpoints in the directory ending with .pth
+for ckpt_path in $ckpt_dir/*.pth; do
+
+    echo $ckpt_path
+
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+done
+
+
+###################### Exp 18 ########################################
+echo "Exp 18"
+ckpt_dir="Results/Baseline/resnet50_exp18"
+
+echo "ckpt_dir: $ckpt_dir"
+echo "saving results to: $ckpt_dir/eval_knn_results"
+
+# loop over all the checkpoints in the directory ending with .pth
+for ckpt_path in $ckpt_dir/*.pth; do
+
+    echo $ckpt_path
+
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+done
+
+
+###################### Exp 19 ########################################
+echo "Exp 19"
+ckpt_dir="Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_exp19"
+
+echo "ckpt_dir: $ckpt_dir"
+echo "saving results to: $ckpt_dir/eval_knn_results"
+
+# loop over all the checkpoints in the directory ending with .pth
+for ckpt_path in $ckpt_dir/*.pth; do
+
+    echo $ckpt_path
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+done
+
+###################### Exp 19 ########################################
+echo "Exp 19"
+ckpt_dir="Results/Adv/resnet50_dynamicaug_true_epsilon_warmup_5000_only_adv_exp19"
+
+echo "ckpt_dir: $ckpt_dir"
+echo "saving results to: $ckpt_dir/eval_knn_results"
+
+# loop over all the checkpoints in the directory ending with .pth
+for ckpt_path in $ckpt_dir/*.pth; do
+
+    echo $ckpt_path
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+done
+
+###################### Exp 19 ########################################
+echo "Exp 19"
 ckpt_dir="Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_exp19"
 
 echo "ckpt_dir: $ckpt_dir"
@@ -97,11 +191,9 @@ for ckpt_path in $ckpt_dir/*.pth; do
 
 done
 
-
-
-
-
-ckpt_dir="Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_exp24_with_embedding256"
+###################### Exp 24 ########################################
+echo "Exp 24"
+ckpt_dir="Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_exp24_with_embedding256"
 
 echo "ckpt_dir: $ckpt_dir"
 echo "saving results to: $ckpt_dir/eval_knn_results"
@@ -110,16 +202,16 @@ echo "saving results to: $ckpt_dir/eval_knn_results"
 for ckpt_path in $ckpt_dir/*.pth; do
 
     echo $ckpt_path
-    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name --model_num_embedding_out 256  \
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
     --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
-    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name --model_num_embedding_out 256  \
+    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
     --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
 
 done
 
-
-
-ckpt_dir="Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_exp25_with_embedding512/"
+###################### Exp 25 ########################################
+echo "Exp 25"
+ckpt_dir="Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_exp25_with_embedding512"
 
 echo "ckpt_dir: $ckpt_dir"
 echo "saving results to: $ckpt_dir/eval_knn_results"
@@ -128,9 +220,87 @@ echo "saving results to: $ckpt_dir/eval_knn_results"
 for ckpt_path in $ckpt_dir/*.pth; do
 
     echo $ckpt_path
-    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name --model_num_embedding_out 512  \
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
     --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
-    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name --model_num_embedding_out 512  \
+    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
     --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
 
 done
+
+
+###################### Exp 26 ########################################
+echo "Exp 26"
+ckpt_dir="Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_exp26_with_embedding768"
+
+echo "ckpt_dir: $ckpt_dir"
+echo "saving results to: $ckpt_dir/eval_knn_results"
+
+# loop over all the checkpoints in the directory ending with .pth
+for ckpt_path in $ckpt_dir/*.pth; do
+
+    echo $ckpt_path
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+done
+
+
+###################### Exp 27 ########################################
+echo "Exp 27"
+ckpt_dir="Results/Adv/resnet50_timm_pretrained_dynamicaug_true_epsilon_warmup_5000_only_adv_exp27_with_embedding1024"
+
+echo "ckpt_dir: $ckpt_dir"
+echo "saving results to: $ckpt_dir/eval_knn_results"
+
+# loop over all the checkpoints in the directory ending with .pth
+for ckpt_path in $ckpt_dir/*.pth; do
+
+    echo $ckpt_path
+    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+
+done
+
+
+#
+#
+#
+#
+#
+#ckpt_dir="Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_exp24_with_embedding256"
+#
+#echo "ckpt_dir: $ckpt_dir"
+#echo "saving results to: $ckpt_dir/eval_knn_results"
+#
+## loop over all the checkpoints in the directory ending with .pth
+#for ckpt_path in $ckpt_dir/*.pth; do
+#
+#    echo $ckpt_path
+#    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name --model_num_embedding_out 256  \
+#    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+#    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name --model_num_embedding_out 256  \
+#    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+#
+#done
+#
+#
+#
+#ckpt_dir="Results/Adv/resnet50_at_dynamicaug_true_epsilon_warmup_5000_only_adv_exp25_with_embedding512/"
+#
+#echo "ckpt_dir: $ckpt_dir"
+#echo "saving results to: $ckpt_dir/eval_knn_results"
+#
+## loop over all the checkpoints in the directory ending with .pth
+#for ckpt_path in $ckpt_dir/*.pth; do
+#
+#    echo $ckpt_path
+#    python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name --model_num_embedding_out 512  \
+#    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+#    python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name --model_num_embedding_out 512  \
+#    --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+#
+#done
