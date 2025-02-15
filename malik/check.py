775963414f624faf16fc14f41bfc2e1d57a86ed1
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2025-02-15 12:07:50 +0400

    update

--- malik/check.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if __name__ == "__main__":
-    ckpt = torch.load("best_clean_loss_checkpoint.pth")
-    print(ckpt["epoch"])
-    print(ckpt["loss"])
-    ckpt = torch.load("best_adv_loss_checkpoint.pth")
-    print(ckpt["epoch"])
-    print(ckpt["loss"])
