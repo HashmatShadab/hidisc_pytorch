d8e46ca3cbf54527066c16afd30d6511c79c38e8
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2025-05-11 11:23:30 +0400

    add

--- scripts/eval_adv_knn_40k_any_attack.sh
+++ scripts/eval_adv_knn_40k_any_attack.sh
@@ -93,24 +93,24 @@ if [ $EXP_NUMBER -eq 18 ]; then
 #      fi
 
 
-  model_name="wresnet50_normal"
-  ckpt_dir="/mnt/nvme0n1/Dataset/muzammal/hidisc_results/Results/Baseline/wresnet50_normal_exp18"
-  ckpt_path=$ckpt_dir/checkpoint_40000.pth
+  # model_name="wresnet50_normal"
+  # ckpt_dir="/mnt/nvme0n1/Dataset/muzammal/hidisc_results/Results/Baseline/wresnet50_normal_exp18"
+  # ckpt_path=$ckpt_dir/checkpoint_40000.pth
 
-  echo "Exp 18 with $model_name"
-  echo "ckpt_dir: $ckpt_dir"
-  echo "saving results to: $ckpt_dir/eval_knn_results"
-  echo "ckpt_path: $ckpt_path"
+  # echo "Exp 18 with $model_name"
+  # echo "ckpt_dir: $ckpt_dir"
+  # echo "saving results to: $ckpt_dir/eval_knn_results"
+  # echo "ckpt_path: $ckpt_path"
 
 
 
-       if [ $adv_eval == "true" ]; then
-        CUDA_VISIBLE_DEVICES=$GPU python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
-        --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results --eps $epsilon --steps $steps --eval_predict_batch_size $batch_size --attack_name $attack_name --eval_predict_batch_size $batch_size --attack_name $attack_name
-      else
-        CUDA_VISIBLE_DEVICES=$GPU python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
-        --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
-      fi
+  #      if [ $adv_eval == "true" ]; then
+  #       CUDA_VISIBLE_DEVICES=$GPU python adv_eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+  #       --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results --eps $epsilon --steps $steps --eval_predict_batch_size $batch_size --attack_name $attack_name --eval_predict_batch_size $batch_size --attack_name $attack_name
+  #     else
+  #       CUDA_VISIBLE_DEVICES=$GPU python eval_knn.py --data_db_root $DATA_PATH  --model_backbone $model_name   \
+  #       --eval_ckpt_path $ckpt_path  --save_results_path $ckpt_dir/eval_knn_results
+  #     fi
 
 
       ###################### Exp 18 ########################################
